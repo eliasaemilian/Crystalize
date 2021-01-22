13cb561448f8528aa3feec646d35d6282c5dc054
Elias <caprisnoctis@googlemail.com>
2021-01-22 21:51:55 +0100

    added mesh triangle gen to geometry shader

--- Crystalize/Assets/Crystals/CrystalComputeInit.cs
+++ Crystalize/Assets/Crystals/CrystalComputeInit.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CrystalComputeInit : MonoBehaviour
 {
     public ComputeShader shader;
-    public Material mat;
+    public Material mat; // geometry shader
 
     struct Crystal
     {
@@ -23,6 +23,7 @@ public class CrystalComputeInit : MonoBehaviour
 
     ComputeBuffer verticesBuffer;
     ComputeBuffer uvsBuffer;
+    ComputeBuffer indicesBuffer;
     ComputeBuffer crystalInfoBuffer;
 
     int verticesNum = 0;
@@ -34,7 +35,6 @@ public class CrystalComputeInit : MonoBehaviour
     void Start()
     {
         computeCrystal();
-       
     }
 
     // Update is called once per frame
@@ -47,6 +47,7 @@ public class CrystalComputeInit : MonoBehaviour
     {
         if (verticesBuffer != null) verticesBuffer.Dispose();
         if (uvsBuffer != null) uvsBuffer.Dispose();
+        if (indicesBuffer != null) indicesBuffer.Dispose();
         if (crystalInfoBuffer != null) crystalInfoBuffer.Dispose();
     }
 
@@ -65,6 +66,12 @@ public class CrystalComputeInit : MonoBehaviour
         float pointiness = Random.Range( runningConfig.MinPointiness, runningConfig.MaxPointiness );
 
         verticesNum = length * segments * 2;
+        indicesCount = ( 6 * ( length - 1 ) ) + ( ( length - 1 ) ) * ( 3 * ( ( segments * 2 ) - 2 ) );
+
+        // set geo shader vars
+        mat.SetFloat( "_VerticesCount", verticesNum );
+        mat.SetFloat( "_IndicesCount", indicesCount );
+
 
         Crystal testCrystal = new Crystal();
         testCrystal.radius = radius;
@@ -80,6 +87,7 @@ public class CrystalComputeInit : MonoBehaviour
         verticesBuffer = new ComputeBuffer( verticesNum, sizeof( float ) * 3, ComputeBufferType.Default );
         uvsBuffer = new ComputeBuffer( verticesNum, sizeof( float ) * 2, ComputeBufferType.Default );
         crystalInfoBuffer = new ComputeBuffer( 1, sizeof( float ) * 4 + sizeof (float) * 3, ComputeBufferType.Default );
+        indicesBuffer = new ComputeBuffer( indicesCount, sizeof( uint ), ComputeBufferType.Default );
 
         // Set Mesh Config Data
         crystalInfoBuffer.SetData( data );
@@ -90,7 +98,7 @@ public class CrystalComputeInit : MonoBehaviour
         shader.SetBuffer( kernelHandle, "VerticesBuffer", verticesBuffer );
         shader.SetBuffer( kernelHandle, "UvsBuffer", uvsBuffer );
         shader.SetBuffer( kernelHandle, "CrystalInfoBuffer", crystalInfoBuffer );
-       // shader.SetBuffer( kernelHandle, "IndicesBuffer", indicesBuffer );
+        shader.SetBuffer( kernelHandle, "IndicesBuffer", indicesBuffer );
 
 
 
@@ -98,6 +106,13 @@ public class CrystalComputeInit : MonoBehaviour
         // DISPATCH
 
         shader.Dispatch( kernelHandle, 1, 1, 1 );
+        // SET BUFFERS FOR GEO SHADER
+        mat.SetBuffer( "VerticesBuffer", verticesBuffer );
+        mat.SetBuffer( "IndicesBuffer", indicesBuffer );
+        mat.SetBuffer( "UvsBuffer", uvsBuffer );
+
+        //Graphics.DrawProcedural( mat, mesh.bounds, MeshTopology.Points, 128);
+
 
         Vector3[] vertOut = new Vector3[verticesNum];
         verticesBuffer.GetData( vertOut );
@@ -105,129 +120,28 @@ public class CrystalComputeInit : MonoBehaviour
         Vector2[] uvsOut = new Vector2[verticesNum];
         uvsBuffer.GetData( uvsOut );
 
+        int[] indisOut = new int[indicesCount];
+        indicesBuffer.GetData( indisOut );
+
         ComputeVertices.AddRange( vertOut );
-        Debug.Log( "Completed Compute Step" );
-
-        // COMPARISON
-
-        // DEBUG VALUES
-        Vector3 gen = Vector3.zero;
 
 
-        // VERTICES
-        List<Vector3> vertices = new List<Vector3>();
+        
+
         List<Vector3> normals = new List<Vector3>();
-        List<Vector2> uvs = new List<Vector2>();
-        for ( int i = 0; i < length; i++ )
-        {
-            for ( int j = 0; j < segments; j++ )
-            {
-                Vector3 vertPos;
-                float te;
-                if ( i == length - 1 ) // top point for crystal (double row)
-                {
-                    vertPos = transform.position;
-                    vertPos.y += i + pointiness;
-                    te = 10;
-                }
-                else
-                {
-
-                    float t = j / (float)segments;
-                    float angRad = t * Mathfs.TAU; // angle in radians   
-
-                    // Get Basic Corpus
-                    Vector2 dir = Mathfs.GetUnitVectorByAngle( angRad );
-
-                    vertPos = dir * radius;
-                    gen = dir;
-                    te = t;
-
-                    vertPos = Quaternion.Euler( 90, 0, 0 ) * vertPos; // rotate to point crystal upwards
-                    vertPos.y += i; //enlongate on y
-
-                }
-
-                vertices.Add( vertPos );
-                vertices.Add( new Vector3( 1000000, 10000000, 10000000 ) );
-                float v = vertPos.y / ( length + pointiness );
-                float u = j / (float)segments;
-                uvs.Add( new Vector2( u, v ) );
-                uvs.Add( new Vector2( u, v ) );
-
-            }
-
-
-        }
-
-
-
-        ////////////////
-        ///
-        GeneratedVertices = vertices;
-
-        Debug.Log( $"Generated X: {vertices[2].x} Y: {vertices[2].y} Z: {vertices[2].z}" );
-        Debug.Log( $"Computed X: {ComputeVertices[2].x} Y: {ComputeVertices[2].y} Z: {ComputeVertices[2].z}" );
-        Debug.Log( $"Generated Prior: X: {gen.x} Y: {gen.y} Z: {gen.z}" );
-        Debug.Log( $"Computed Propr:  X: {ComputeVertices[1].x} Y: {ComputeVertices[1].y} Z: {ComputeVertices[1].z}" );
-
-        for ( int i = 0; i < vertices.Count; i+= 2 )
-        {
-            Debug.Log( $"Computed X: {ComputeVertices[i].x} Generated X:{vertices[i].x} " );
-        }
-        /////////////////////////
-
-
-        List<int> indices = new List<int>();
-
-        // INDICES
-        for ( int i = 0; i < length - 1; i++ )
-        {
-            for ( int j = 0; j < ( segments * 2 ) - 2; j += 2 )
-            {
-                int root = ( i * ( ( segments * 2 ) ) + j );
-                int rootNext = ( i * ( ( segments * 2 ) ) ) + j + ( segments * 2 );
-
-                // 1
-                indices.Add( rootNext );
-                indices.Add( root + 2 );
-                indices.Add( root );
-
-                // 2
-                indices.Add( rootNext );
-                indices.Add( rootNext + 2 );
-                indices.Add( root + 2 );
-
-                //  Debug.Log($"root is {root} and root + 1 is {root + 1} and rootNext is {rootNext} and rootNext + 1 in {rootNext + 1}");
-            }
-
-            // close off crystal between first and last row in indices in this column
-
-            //1
-            indices.Add( ( ( i + 1 ) * ( segments * 2 ) ) + ( segments * 2 ) - 2 );
-            indices.Add( ( i * ( segments * 2 ) ) + ( segments * 2 ) );
-            indices.Add( ( i * ( segments * 2 ) ) );
-
-            // 2
-            indices.Add( ( ( i + 1 ) * ( segments * 2 ) ) + ( segments * 2 ) - 2 );
-            indices.Add( ( i * ( segments * 2 ) ) );
-            indices.Add( ( i * ( segments * 2 ) ) + ( segments * 2 ) - 2 );
-
-        }
-
-
 
         // Set Mesh
-        mesh.SetVertices( ComputeVertices );
-        mesh.SetTriangles( indices, 0 );
+        Vector3[] verts = new Vector3[6];
+        int[] indis = new int[3];
+        mesh.SetVertices( vertOut );
+        mesh.SetTriangles( indisOut, 0 );
         //  mesh.SetNormals(normals);
-        mesh.SetUVs( 0, uvsOut );
-        mesh.RecalculateNormals();
+        //   mesh.SetUVs( 0, uvsOut );
+        //mesh.RecalculateNormals();
 
         crystal.GetComponent<MeshFilter>().mesh = mesh;
         //  crystal.GetComponent<MeshRenderer>().material = new Material( GetComponent<MeshRenderer>().material );
         crystal.GetComponent<MeshRenderer>().material = mat;
-        indicesCount = indices.Count;
         return crystal;
     }
 }
---
+++ Crystalize/Assets/Crystals/GeometryTest.shader
@@ -0,0 +1,108 @@
+﻿Shader "Unlit/GeometryTest"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _VerticesCount ("Vert Count", float) = 10
+        _IndicesCount ("Indices Count", float) = 10
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma geometry geom
+
+            // make fog work
+            #pragma multi_compile_fog
+            #pragma target 5.0
+            #pragma multi_compile_instancing
+            #include "UnityCG.cginc"
+
+
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+
+            };
+
+            struct v2g
+            {
+                float4 objPos : SV_POSITION;
+                float2 uv : TEXCOORD0;
+
+             //   float3 normal : NORMAL;
+            };
+
+            struct g2f
+            {
+                float4 vertex : SV_POSITION;
+              //  float2 uv : TEXCOORD0;
+              //  fixed4 color : COLOR;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+            float _VerticesCount;
+
+            StructuredBuffer<float3> VerticesBuffer;
+            StructuredBuffer<float2> UvsBuffer;
+            StructuredBuffer<uint> IndicesBuffer;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                UNITY_TRANSFER_FOG(o,o.vertex);
+                return o;
+            }
+
+            [maxvertexcount( 256 )] [instance( 32 )]
+            void geom( triangle v2g IN[3], inout TriangleStream<g2f> tristream, uint InstanceID : SV_GSInstanceID)
+            {
+                g2f o;
+                //
+                for (uint i = 0; i < _VerticesCount; i++)
+                {
+                    uint count = 256 * InstanceID + i;
+                    o.vertex = UnityObjectToClipPos( float4( VerticesBuffer[ IndicesBuffer[count]], 0 ) );
+                    //o.color = fixed4( .5, .5, .0, 1 );
+                  //  o.uv = Buffer[IndicesBuffer[count]];
+                    tristream.Append( o );
+
+
+                }
+
+                
+            }
+
+
+
+            fixed4 frag (g2f i) : SV_Target
+            {
+                // sample the texture
+             //   fixed4 col = tex2D(_MainTex, i.uv);
+                // apply fog
+                UNITY_APPLY_FOG(i.fogCoord, col);
+                return fixed4(.1,0,.4,1);
+            }
+            ENDCG
+        }
+    }
+}

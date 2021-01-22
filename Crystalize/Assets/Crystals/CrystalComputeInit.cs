94b592f6a01478e95c58bb1cc580e72c01835e94
Elias <caprisnoctis@googlemail.com>
2021-01-22 13:35:31 +0100

    completed single crystal mesh gpu instan

--- Crystalize/Assets/Crystals/CrystalComputeInit.cs
+++ Crystalize/Assets/Crystals/CrystalComputeInit.cs
@@ -13,6 +13,7 @@ public class CrystalComputeInit : MonoBehaviour
         public float length; // do random calcs on cpu
         public float segments; // do random calcs on cpu
         public float pointiness;
+        public Vector3 wPos;
     };
 
     public CrystalConfig runningConfig;
@@ -70,6 +71,7 @@ public class CrystalComputeInit : MonoBehaviour
         testCrystal.length = length;
         testCrystal.pointiness = pointiness;
         testCrystal.segments = segments;
+        testCrystal.wPos = transform.position;
 
         Crystal[] data = new Crystal[1];
         data[0] = testCrystal;
@@ -77,7 +79,7 @@ public class CrystalComputeInit : MonoBehaviour
         // INIT BUFFERS
         verticesBuffer = new ComputeBuffer( verticesNum, sizeof( float ) * 3, ComputeBufferType.Default );
         uvsBuffer = new ComputeBuffer( verticesNum, sizeof( float ) * 2, ComputeBufferType.Default );
-        crystalInfoBuffer = new ComputeBuffer( 1, sizeof( float ) * 4, ComputeBufferType.Default );
+        crystalInfoBuffer = new ComputeBuffer( 1, sizeof( float ) * 4 + sizeof (float) * 3, ComputeBufferType.Default );
 
         // Set Mesh Config Data
         crystalInfoBuffer.SetData( data );
@@ -89,15 +91,28 @@ public class CrystalComputeInit : MonoBehaviour
         shader.SetBuffer( kernelHandle, "UvsBuffer", uvsBuffer );
         shader.SetBuffer( kernelHandle, "CrystalInfoBuffer", crystalInfoBuffer );
        // shader.SetBuffer( kernelHandle, "IndicesBuffer", indicesBuffer );
-        shader.Dispatch( kernelHandle, 8, 8, 1 );
+
+
+
+
+        // DISPATCH
+
+        shader.Dispatch( kernelHandle, 1, 1, 1 );
 
         Vector3[] vertOut = new Vector3[verticesNum];
         verticesBuffer.GetData( vertOut );
+
+        Vector2[] uvsOut = new Vector2[verticesNum];
+        uvsBuffer.GetData( uvsOut );
 
         ComputeVertices.AddRange( vertOut );
         Debug.Log( "Completed Compute Step" );
 
         // COMPARISON
+
+        // DEBUG VALUES
+        Vector3 gen = Vector3.zero;
+
 
         // VERTICES
         List<Vector3> vertices = new List<Vector3>();
@@ -117,6 +132,7 @@ public class CrystalComputeInit : MonoBehaviour
                 }
                 else
                 {
+
                     float t = j / (float)segments;
                     float angRad = t * Mathfs.TAU; // angle in radians   
 
@@ -124,15 +140,16 @@ public class CrystalComputeInit : MonoBehaviour
                     Vector2 dir = Mathfs.GetUnitVectorByAngle( angRad );
 
                     vertPos = dir * radius;
+                    gen = dir;
                     te = t;
 
                     vertPos = Quaternion.Euler( 90, 0, 0 ) * vertPos; // rotate to point crystal upwards
-                    //  vertPos.y += i; //enlongate on y
+                    vertPos.y += i; //enlongate on y
 
                 }
 
                 vertices.Add( vertPos );
-                vertices.Add( new Vector3( te, te, te ) );
+                vertices.Add( new Vector3( 1000000, 10000000, 10000000 ) );
                 float v = vertPos.y / ( length + pointiness );
                 float u = j / (float)segments;
                 uvs.Add( new Vector2( u, v ) );
@@ -144,12 +161,21 @@ public class CrystalComputeInit : MonoBehaviour
         }
 
 
+
         ////////////////
         ///
         GeneratedVertices = vertices;
 
+        Debug.Log( $"Generated X: {vertices[2].x} Y: {vertices[2].y} Z: {vertices[2].z}" );
+        Debug.Log( $"Computed X: {ComputeVertices[2].x} Y: {ComputeVertices[2].y} Z: {ComputeVertices[2].z}" );
+        Debug.Log( $"Generated Prior: X: {gen.x} Y: {gen.y} Z: {gen.z}" );
+        Debug.Log( $"Computed Propr:  X: {ComputeVertices[1].x} Y: {ComputeVertices[1].y} Z: {ComputeVertices[1].z}" );
 
-
+        for ( int i = 0; i < vertices.Count; i+= 2 )
+        {
+            Debug.Log( $"Computed X: {ComputeVertices[i].x} Generated X:{vertices[i].x} " );
+        }
+        /////////////////////////
 
 
         List<int> indices = new List<int>();
@@ -192,10 +218,10 @@ public class CrystalComputeInit : MonoBehaviour
 
 
         // Set Mesh
-        mesh.SetVertices( vertices );
+        mesh.SetVertices( ComputeVertices );
         mesh.SetTriangles( indices, 0 );
         //  mesh.SetNormals(normals);
-        mesh.SetUVs( 0, uvs );
+        mesh.SetUVs( 0, uvsOut );
         mesh.RecalculateNormals();
 
         crystal.GetComponent<MeshFilter>().mesh = mesh;
